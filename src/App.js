a3d1fae296fec2d0fa55320b412120b46c024c05
zolomohan <mohanzolomon@gmail.com>
2020-12-18 03:13:07 +0530

    Add Starter Code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import SwitchButton from "./Button";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`bg`}>
+      <h1 className={`heading`}>Light Mode</h1>
+      <p className={`para`}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Sed tempus urna et pharetra pharetra massa massa ultricies. Id porta nibh venenatis cras sed felis
+        eget velit. A diam sollicitudin tempor id eu. Tincidunt arcu non sodales neque sodales ut etiam sit. Eu
+        scelerisque felis imperdiet proin fermentum. Faucibus a pellentesque sit amet porttitor. Laoreet id donec
+        ultrices tincidunt arcu non sodales neque. Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla facilisi
+        cras. Sollicitudin tempor id eu nisl nunc. Eget velit aliquet sagittis id consectetur purus ut. Cras pulvinar
+        mattis nunc sed blandit libero volutpat. Amet consectetur adipiscing elit pellentesque habitant morbi tristique
+        senectus et. Sed ullamcorper morbi tincidunt ornare massa. Et egestas quis ipsum suspendisse ultrices gravida
+        dictum fusce. Cras pulvinar mattis nunc sed blandit libero volutpat sed cras. Diam sollicitudin tempor id eu.
+        Sed pulvinar proin gravida hendrerit lectus. Dapibus ultrices in iaculis nunc sed augue lacus viverra vitae.
+        Eget nunc lobortis mattis aliquam faucibus purus. Mi sit amet mauris commodo quis imperdiet massa tincidunt.
+        Eget gravida cum sociis natoque penatibus et magnis dis. In massa tempor nec feugiat nisl pretium fusce. Nulla
+        at volutpat diam ut venenatis tellus in metus vulputate. Purus gravida quis blandit turpis cursus in. Nulla
+        porttitor massa id neque aliquam. Metus aliquam eleifend mi in nulla posuere. Consequat id porta nibh venenatis
+        cras sed. A lacus vestibulum sed arcu non. Lacus viverra vitae congue eu consequat ac felis donec et. Vitae et
+        leo duis ut. Consequat interdum varius sit amet mattis vulputate enim nulla. Faucibus scelerisque eleifend donec
+        pretium vulputate sapien nec.
+      </p>
+      <SwitchButton />
     </div>
   );
 }
---
+++ src/Button.js
@@ -0,0 +1,6 @@
+import "./App.css";
+import React from "react";
+
+export default function SwitchButton(props) {
+  return <button className={`btn btn-light`}>Switch to Dark Mode</button>;
+}
