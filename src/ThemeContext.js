1c8e5c852b05463a4c9c4840b80a401fde5ec7c0
zolomohan <mohanzolomon@gmail.com>
2020-12-26 18:16:52 +0530

    Add Context Object & Provider

---
+++ src/ThemeContext.js
@@ -0,0 +1,24 @@
+import React, { createContext, useReducer } from "react";
+
+export const ThemeContext = createContext();
+
+const initialState = {
+  darkMode: false,
+};
+
+const themeReducer = (state, action) => {
+  switch (action.type) {
+    case "LIGHTMODE":
+      return { darkMode: false };
+    case "DARKMODE":
+      return { darkMode: true };
+    default:
+      return state;
+  }
+};
+
+export function ThemeProvider(props) {
+  const [state, dispatch] = useReducer(themeReducer, initialState);
+
+  return <ThemeContext.Provider value={{ state, dispatch }}>{props.children}</ThemeContext.Provider>;
+}
